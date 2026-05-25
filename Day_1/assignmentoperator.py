9f5e36fd934c9238186ba61232ed049b09e8db8c
nishita4510 <2023.ankital@isu.ac.in>
2026-05-26 00:58:12 +0530

    i learned input function in python

--- Day_1/assignmentoperator.py
+++ Day_1/assignmentoperator.py
@@ -1,7 +1,7 @@
 name = "Ankita Lokhande"
 age1 = 23
 age2 = 40
-print (age2)
+print ("Actual Value:", age2)
 favSubject = "Maths"
 age2 = age1
-print (age2)
+print ("Changed Value:",age2)
---
+++ Day_2/userinpur.py
@@ -0,0 +1,10 @@
+#Program to take input from the user
+
+print("a sample method to udnderstand user input in python")
+
+name=input("Enter your  good name:")
+age=input("Enter your age:")
+print("Your good name is:"+name )
+print("Your age is:"+ age)
+print("My overall details are my name :" +name)
+print("And my age: " +age)
